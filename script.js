efff458d4206bf4b28ed99b76bfe9f3c42006b7c
gabrieltomazi <tomazigabriel3@gmail.com>
2026-02-18 13:30:32 -0300

    fix: ajuste responsividade

--- script.js
+++ script.js
@@ -10,9 +10,44 @@ ScrollSmoother.create({
 });
 
 
+gsap.from('.projetos-card', {
+    opacity: 0,
+    y: 150,
+    stagger: .3,
+    scrollTrigger: {
+        scrub: true,
+        trigger: '.projetos-card',
+        start: 'top 80%',
+        end: 'top 50%'
+    }
+})
+
+
+document.querySelectorAll('.projetos-card').forEach(card => {
+    card.addEventListener('mouseenter', () => {
+        gsap.to(card, {
+            y: -15,
+            duration: 0.3,
+            boxShadow: '0 10px 20px rgba(79, 70, 229, 0.2)'
+
+        });
+    });
+
+    card.addEventListener('mouseleave', () => {
+        gsap.to(card, {
+            y: 0,
+            boxShadow: "0 0px 0px rgba(0, 0, 0, 0)",
+            duration: 0.3,
+
+        });
+    });
+});
+
+
+
 document.querySelectorAll('.menu-link').forEach(link => {
     link.addEventListener('click', (e) => {
-        e.preventDefault(); 
+        e.preventDefault();
         const target = e.target.getAttribute('href');
 
         gsap.to(window, {
